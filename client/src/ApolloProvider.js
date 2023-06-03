cbcd55c57d6e9b99d46f8efb56e69eb754c26ecb
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-06-03 14:53:53 +0200

    creating the app and setting up Apollo

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import App from './App'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
+function AppProvider() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  )
+}
+
+export default AppProvider
