b0c155a934b4fa61c8302f88927416b8f4f59735
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-06-03 15:07:20 +0200

    implemented Semantic-UI, created routed pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
+
+import MenuBar from './components/MenuBar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div>
-      <h1>Hi world!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Container>
+    </Router>
   )
 }
 
