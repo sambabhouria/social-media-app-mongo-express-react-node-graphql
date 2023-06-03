ad37149268d8ecfba50c4fbbbd233e3098be9f3b
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-06-03 16:22:18 +0200

    Displaying posts on the home page

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import App from './App'
-import ApolloClient from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { createHttpLink } from 'apollo-link-http'
-import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+// import ApolloClient from 'apollo-client'
+// import { InMemoryCache } from 'apollo-cache-inmemory'
+// import { createHttpLink } from 'apollo-link-http'
+// import { ApolloProvider } from '@apollo/react-hooks'
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000',
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:4000',
+// })
+
+// const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// })
+
+//  InMemoryCache : add client without reload page
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+      },
+    },
+  },
 })
 
 const client = new ApolloClient({
-  link: httpLink,
+  uri: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/',
   cache: new InMemoryCache(),
 })
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,55 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/client'
+import { Grid } from 'semantic-ui-react'
+
+import PostCard from '../components/PostCard'
 
 function Home() {
+  const { loading, error, data } = useQuery(FETCH_POSTS_QUERY)
+
+  const posts = data?.getPosts || []
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid columns={3}>
+      {error && <p>Something Went Wrong</p>}
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   )
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`
 
 export default Home
